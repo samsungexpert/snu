eaf227ecde3c0c2e1c2c0416336600ecdf231066
BWJung <bw@MacPro.local>
2022-08-24 01:44:23 +0900

    [BW] default training mode

--- mytrain_tf.py
+++ mytrain_tf.py
@@ -277,7 +277,7 @@ if __name__ == '__main__':
     parser.add_argument(
             '--test',
             type=bool,
-            default=True,
+            default=False,
             help='test')
 
     args = parser.parse_args()
